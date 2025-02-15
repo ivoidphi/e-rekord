7a9b381269566a4c60604a29d1b5bd155ff4f700
ivoidphi <69293191+ivoidphi@users.noreply.github.com>
2025-02-15 13:37:41 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function showlogs(){
+    document.getElementById("productsTable").style.display = "none";
+    document.getElementById("logsTable").style.display = "block";
+    document.getElementById("accountsTable").style.display = "none";
+    document.getElementById("productsButton").classList.remove("active");  
+    document.getElementById("logsButton").classList.add("active");
+    document.getElementById("accountsButton").classList.remove("active"); 
+  
+}
+
+function showproducts(){
+    document.getElementById("productsTable").style.display = "block";
+    document.getElementById("logsTable").style.display = "none";
+    document.getElementById("accountsTable").style.display = "none";
+    document.getElementById("productsButton").classList.add("active");
+    document.getElementById("logsButton").classList.remove("active");
+    document.getElementById("accountsButton").classList.remove("active"); 
+   
+}
+
+function showaccounts(){
+    document.getElementById("productsTable").style.display = "none";
+    document.getElementById("logsTable").style.display = "none";
+    document.getElementById("accountsTable").style.display = "block";
+    document.getElementById("productsButton").classList.remove("active");
+    document.getElementById("logsButton").classList.remove("active"); 
+    document.getElementById("accountsButton").classList.add("active");  
+}
+
+function toggleMenu(){
+    const showMenu = document.getElementById("leftsection").style.display;
+
+    if(showMenu == "block"){
+        document.getElementById("leftsection").style.display = "none";
+    }else{
+        document.getElementById("leftsection").style.display = "block";
+    }
+}
