60a9b7d8941d5dda7593622bc29226055b1f5527
ivoidphi <mintvoid123@gmail.com>
2025-02-15 15:51:27 +0800

    3:51PM

--- script.js
+++ script.js
@@ -2,29 +2,44 @@ function showlogs(){
     document.getElementById("productsTable").style.display = "none";
     document.getElementById("logsTable").style.display = "block";
     document.getElementById("accountsTable").style.display = "none";
+    document.getElementById("dataTable").style.display = "none";
     document.getElementById("productsButton").classList.remove("active");  
     document.getElementById("logsButton").classList.add("active");
-    document.getElementById("accountsButton").classList.remove("active"); 
-  
+    document.getElementById("accountsButton").classList.remove("active");
+    document.getElementById("dataButton").classList.remove("active");
 }
 
 function showproducts(){
     document.getElementById("productsTable").style.display = "block";
     document.getElementById("logsTable").style.display = "none";
     document.getElementById("accountsTable").style.display = "none";
+    document.getElementById("dataTable").style.display = "none";
     document.getElementById("productsButton").classList.add("active");
     document.getElementById("logsButton").classList.remove("active");
-    document.getElementById("accountsButton").classList.remove("active"); 
-   
+    document.getElementById("accountsButton").classList.remove("active");
+    document.getElementById("dataButton").classList.remove("active");
 }
 
 function showaccounts(){
     document.getElementById("productsTable").style.display = "none";
     document.getElementById("logsTable").style.display = "none";
     document.getElementById("accountsTable").style.display = "block";
+    document.getElementById("dataTable").style.display = "none";
     document.getElementById("productsButton").classList.remove("active");
     document.getElementById("logsButton").classList.remove("active"); 
-    document.getElementById("accountsButton").classList.add("active");  
+    document.getElementById("accountsButton").classList.add("active");
+    document.getElementById("dataButton").classList.remove("active");
+}
+
+function showdata() {
+    document.getElementById("productsTable").style.display = "none";
+    document.getElementById("logsTable").style.display = "none";
+    document.getElementById("accountsTable").style.display = "none";
+    document.getElementById("dataTable").style.display = "block";
+    document.getElementById("productsButton").classList.remove("active");
+    document.getElementById("logsButton").classList.remove("active");
+    document.getElementById("accountsButton").classList.remove("active");
+    document.getElementById("dataButton").classList.add("active");
 }
 
 function toggleMenu(){
@@ -32,7 +47,9 @@ function toggleMenu(){
 
     if(showMenu == "block"){
         document.getElementById("leftsection").style.display = "none";
+        document.getElementById("toggleMenu").style.minWidth = "2rem";
     }else{
         document.getElementById("leftsection").style.display = "block";
+        document.getElementById("toggleMenu").style.minWidth = "5rem";
     }
 }
